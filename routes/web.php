d9ddedaa2e37efc2efd8f2b242300c04d3adc49f
thitiwathchang-gif <thitiwath.chang@vru.ac.th>
2025-08-02 12:17:11 +0700

    เพิ่มหน้าทดสอบ

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
+Route::get('/test', function () {
+    return "This is test page.";
+});
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
